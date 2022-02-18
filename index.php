9d4b134e2a344cf50591f0accd09cb0e05cc54d9
“Maxwell <newton.maxwell@gmail.com>
2022-02-18 12:29:14 +0000

    cleaned up html and stylesheet, minor styling adjustments

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
 </head>
 
 <body>
-    <nav class="nav-bar">
+    <nav class="nav-bar flex-row">
 
-        <div class="nav-menu">
+        <div class="nav-menu flex-row">
             <div class="nav-menu-item">
                 <a href="#projects-section">Projects</a>
             </div>
@@ -46,7 +46,7 @@
 
     </nav>
 
-    <header>
+    <header class="flex-column">
 
         <h1>Simple, traffic-based pricing</h1>
         <p>Sign-up for our 30-day trial. No credit card required.</p>
@@ -54,14 +54,14 @@
     </header>
 
     <main>
-        <section class="price-calculator-section">
-            <div class="price-calculator">
-                <div class="calculator-headings">
-                    <div class="page-views-counter">
-                        <p id="page-views-counter">100k</p>
+        <section class="price-calculator-section flex-row">
+            <div class="price-calculator flex-column">
+                <div class="calculator-headings flex-row">
+                    <div class="page-views-counter flex-row">
+                        <p id="page-views-counter">100K</p>
                         <p>page-views</p>
                     </div>
-                    <div class="price-counter">
+                    <div class="price-counter flex-row">
                         <p class="price-counter-text">$</p>
                         <p class="price-counter-text" id="price-counter">16.00</p>
                         <p class="price-month-text">/month</p>
@@ -70,18 +70,20 @@
                 <div class="slider-container">
                     <input type="range" min="0" max="4" value="2" class="slider" id="slider-input">
                 </div>
-                <div class="month-yearly-toggle-div">
+                <div class="month-yearly-toggle-div flex-row">
                     <p>Monthly Billing</p>
                     <label class="yearlyDiscountSwitch">
                         <input type="checkbox" id="monthly-yearly-switch">
                         <span class="discountToggle"></span>
                     </label>
-                    <p>Yearly Billing</p>
-                    <p class="discount-text">25% discount</p>
+                    <div class="flex-row yearly-discount">
+                        <p>Yearly Billing</p>
+                        <p class="discount-text">25% discount</p>
+                    </div>
                 </div>
-                <div class="benefits-parent">
-                    <div class="benefits-div">
-                        <div>
+                <div class="benefits-parent flex-row">
+                    <div class="flex-column">
+                        <div class="benefits flex-row">
                             <img src="check.png" alt="check-mark" class="benefits-check">
                             <p class="benefits-text">Unlimited websites</p>
                         </div>
@@ -103,8 +105,8 @@
 
     </main>
 
-    <footer>
-        <div>
+    <footer class="flex-row">
+        <div class="flex-row">
             <div class="footer-item">
                 <a href="https://www.instagram.com/thehopefulhitchhikers/?hl=en" target="_blank">Instagram</a>
             </div>
